d0cb514fce32a9ecc5a2b6c1e40b48288eef69e5
shivanshu-goswami <goswamishivanshu47@gmail.com>
2026-02-18 17:21:55 +0530

    angular day 1

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,35 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Profile } from './profile/profile';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Profile],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('angular-beginner');
+  // protected readonly title = signal('angular-beginner');
+  // name="shivanshu"
+  // age=20
+
+  // updateName(){
+  //   this.name="Ansh"
+  // }
+  // getCount(a:number,b:number){
+  //   return a+b;
+  // }
+  callMe(){
+    alert("hey there, caught you")
+  }
+
+  count=0;
+  counter(action:string){
+    if(action==="plus"){
+      this.count++;
+    }else{
+      this.count--;
+    }
+  }
+
 }
